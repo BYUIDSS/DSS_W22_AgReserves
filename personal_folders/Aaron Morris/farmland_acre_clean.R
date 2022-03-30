5a830a52c5aa78238abde97e70797f81c25b1b97
aarontmo <mor15015@byui.edu>
2022-03-29 23:22:48 -0600

    farmland acres r cleaning script

---
+++ personal_folders/Aaron Morris/farmland_acre_clean.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+# read in the data
+
+dat <- read_csv('/Users/aaronmorris/Desktop/DSS_W22_AgReserves/personal_folders/Aaron Morris/farmland_acres.csv')
+
+farmland_acres <- dat %>% 
+  select(Name, County, `Total Acres`, `Tillable Acres`) %>% 
+  separate(County, into = c("county", "state"), sep = ",")
+
+farmland_state_acres <- farmland_acres %>% 
+  group_by(state) %>% 
+  summarise(acres = sum(`Total Acres`),
+            tillable_acres = sum(`Tillable Acres`))
+
+write_csv(farmland_acres, "clean_farmland_acres.csv")
+write_csv(farmland_state_acres, "farmland_state_acres.csv")
